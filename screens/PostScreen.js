78fa06893323b60353020977fd18a3102c051f35
Cadu <carlosebfb@gmail.com>
2024-02-24 13:58:23 -0400

    update

--- screens/PostScreen.js
+++ screens/PostScreen.js
@@ -21,8 +21,21 @@ export default class PostScreen extends Component {
     }
 
     componentDidMount() {
-
+        this.fetchUser();
     }
+
+    fetchUser = () => {
+        let theme;
+        const auth = getAuth();
+        const userId = auth.currentUser.uid;
+
+        onValue(ref(db, '/users/' + userId), (snapshot) => {
+            theme = snapshot.val().current_theme;
+            this.setState({
+                light_theme: theme === 'light' ? true : false,
+            });
+        });
+    };
 
     render() {
         if (!this.props.route.params) {
@@ -76,101 +89,123 @@ export default class PostScreen extends Component {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "black"
-    },
-    droidSafeArea: {
-        marginTop: Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
-    },
-    appTitle: {
-        flex: 0.07,
-        flexDirection: "row"
-    },
-    appIcon: {
-        flex: 0.3,
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    iconImage: {
-        width: "100%",
-        height: "100%",
-        resizeMode: "contain"
-    },
-    appTitleTextContainer: {
-        flex: 0.7,
-        justifyContent: "center"
-    },
-    appTitleText: {
-        color: "white",
-        fontSize: RFValue(28)
-    },
-    postContainer: {
-        flex: 1
-    },
-    postCard: {
-        margin: RFValue(20),
-        backgroundColor: "#2a2a2a",
-        borderRadius: RFValue(20)
-    },
-    actionContainer: {
-        justifyContent: "center",
-        alignItems: "center",
-        margin: RFValue(10)
-    },
-    likeButton: {
-        width: RFValue(160),
-        height: RFValue(40),
-        flexDirection: "row",
-        backgroundColor: "#eb3948",
-        justifyContent: "center",
-        alignItems: "center",
-        borderRadius: RFValue(30)
-    },
-    likeText: {
-        color: "white",
-        fontSize: RFValue(25),
-        marginLeft: RFValue(5)
-    },
-    authorContainer: {
-        height: RFPercentage(10),
-        padding: RFValue(10),
-        flexDirection: "row"
-    },
-    authorImageContainer: {
-        flex: 0.15,
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    profileImage: {
-        width: "100%",
-        height: "100%",
-        resizeMode: "contain",
-        borderRadius: RFValue(100)
-    },
-    authorNameContainer: {
-        flex: 0.85,
-        padding: RFValue(10),
-        justifyContent: "center"
-    },
-    authorNameText: {
-        color: "white",
-        fontSize: RFValue(20)
-    },
-    postImage: {
-        width: "100%",
-        alignSelf: "center",
-        height: RFValue(200),
-        borderTopLeftRadius: RFValue(20),
-        borderTopRightRadius: RFValue(20),
-        resizeMode: "contain"
-    },
-    captionContainer: {
-        padding: RFValue(10)
-    },
-    captionText: {
-        fontSize: 13,
-        color: "white",
-        paddingTop: RFValue(10)
-    },
+	container: {
+		flex: 1,
+		backgroundColor: 'black',
+	},
+	containerLight: {
+		flex: 1,
+		backgroundColor: 'white',
+	},
+	droidSafeArea: {
+		marginTop: Platform.OS === 'android' ? StatusBar.currentHeight : RFValue(35),
+	},
+	appTitle: {
+		flex: 0.07,
+		flexDirection: 'row',
+	},
+	appIcon: {
+		flex: 0.3,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	iconImage: {
+		width: '100%',
+		height: '100%',
+		resizeMode: 'contain',
+	},
+	appTitleTextContainer: {
+		flex: 0.7,
+		justifyContent: 'center',
+	},
+	appTitleText: {
+		color: 'white',
+		fontSize: RFValue(28),
+	},
+	appTitleTextLight: {
+		color: 'black',
+		fontSize: RFValue(28),
+	},
+	postContainer: {
+		flex: 1,
+	},
+	postCard: {
+		margin: RFValue(20),
+		backgroundColor: '#2a2a2a',
+		borderRadius: RFValue(20),
+	},
+	postCardLight: {
+		margin: RFValue(20),
+		backgroundColor: '#eaeaea',
+		borderRadius: RFValue(20),
+	},
+	actionContainer: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		margin: RFValue(10),
+	},
+	likeButton: {
+		width: RFValue(160),
+		height: RFValue(40),
+		flexDirection: 'row',
+		backgroundColor: '#eb3948',
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderRadius: RFValue(30),
+	},
+	likeText: {
+		color: 'white',
+		fontSize: RFValue(25),
+		marginLeft: RFValue(5),
+	},
+	authorContainer: {
+		height: RFPercentage(10),
+		padding: RFValue(10),
+		flexDirection: 'row',
+	},
+	authorImageContainer: {
+		flex: 0.15,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	profileImage: {
+		width: '100%',
+		height: '100%',
+		resizeMode: 'contain',
+		borderRadius: RFValue(100),
+	},
+	authorNameContainer: {
+		flex: 0.85,
+		padding: RFValue(10),
+		justifyContent: 'center',
+	},
+	authorNameText: {
+		color: 'white',
+		fontSize: RFValue(20),
+	},
+	authorNameTextLight: {
+		color: 'black',
+		fontSize: RFValue(20),
+	},
+	postImage: {
+		width: '100%',
+		alignSelf: 'center',
+		height: RFValue(200),
+		borderTopLeftRadius: RFValue(20),
+		borderTopRightRadius: RFValue(20),
+		resizeMode: 'contain',
+	},
+	captionContainer: {
+		padding: RFValue(10),
+	},
+	captionText: {
+		fontSize: 13,
+		color: 'white',
+		paddingTop: RFValue(10),
+	},
+	captionTextLight: {
+		fontSize: 13,
+		color: 'black',
+		paddingTop: RFValue(10),
+	},
 });
